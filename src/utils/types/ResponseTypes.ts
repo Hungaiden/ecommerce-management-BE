643c3e5e026c107daed480d344d34a119b708b2d
MANH NGUYEN DUC <manhvttb2004@gmail.com>
2025-04-07 09:15:41 +0700

    hotfix(base): add dto improve controller

---
+++ src/utils/types/ResponseTypes.ts
@@ -0,0 +1,23 @@
+export interface ResponseListSuccess<T> {
+    code: number;
+    data: {
+        hits: T;
+        pagination: {
+            totalRows: number;
+            totalPages: number;
+        };
+    };
+}
+
+export interface ResponseDetailSuccess<T> {
+    code: number;
+    data: T;
+}
+
+export interface ResponseFailure {
+    code: number;
+    timestamp: string;
+    path: string;
+    message: string;
+    errors: any[];
+}
