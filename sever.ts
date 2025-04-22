63e7b00478808cc7454e72b531ccb63b9f36a65e
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2025-04-22 15:00:02 +0700

    add cors setting

--- sever.ts
+++ sever.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -14,6 +15,7 @@ import adminRoutes from "./src/routes/admin/index.route";
 const app: Express = express();
 const port: number = 3000;
 
+app.use(cors());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
@@ -25,6 +27,6 @@ app.locals.prefixAdmin = systemConfig.prefixAdmin;
 adminRoutes(app);
 
 app.listen(port, () => {
-  // eslint-disable-next-line no-undef
-  console.log(`App listening on port ${port}`);
+    // eslint-disable-next-line no-undef
+    console.log(`App listening on port ${port}`);
 });
