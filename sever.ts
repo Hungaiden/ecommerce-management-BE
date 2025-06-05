3240f5d8f63558de8afcdc4103c5598455e38508
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2025-06-05 09:23:53 +0700

    a

--- sever.ts
+++ sever.ts
@@ -1,38 +1,40 @@
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 
-import express, { Express, Request, Response } from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config()
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+dotenv.config();
 
-import bodyParser from 'body-parser'
-import cookieParser from 'cookie-parser'
-import { connect } from './src/config/mongodb'
-connect()
-import { systemConfig } from './src/config/system'
-import adminRoutes from './src/routes/admin/index.route'
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import { connect } from "./src/config/mongodb";
+connect();
+import { systemConfig } from "./src/config/system";
+import adminRoutes from "./src/routes/admin/index.route";
 
-import { corsOptions } from './src/config/cors' 
+import { corsOptions } from "./src/config/cors";
 
-import morgan from 'morgan'
-const app: Express = express()
-const port: number = 3000
+const app: Express = express();
+const port: number = 3000;
 
 // Cau hinh de BE lấy được cookie từ client
-app.use(cookieParser())
+app.use(cookieParser());
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
-app.use(bodyParser.json())
-app.use(morgan('dev'))
-// App Local Variables
-app.locals.prefixAdmin = systemConfig.prefixAdmin
+app.use(bodyParser.json());
 
-adminRoutes(app)
+app.use(morgan("dev"));
+
+// App Local Variables
+app.locals.prefixAdmin = systemConfig.prefixAdmin;
+
+adminRoutes(app);
 
 app.listen(port, () => {
-  // eslint-disable-next-line no-undef
-  console.log(`App listening on port ${port}`)
-})
+    // eslint-disable-next-line no-undef
+    console.log(`App listening on port ${port}`);
+});
--- src/controllers/admin/accounts/auth.controller.ts
+++ src/controllers/admin/accounts/auth.controller.ts
@@ -1,99 +1,101 @@
-import type { Request, Response } from 'express'
-import * as accountService from '../../../services/admin/accounts/auth.service'
-import * as authService from '../../../services/admin/accounts/auth.service'
-import * as paramsTypes from '../../../utils/types/paramsTypes'
-import type { myJwtPayload } from '../../../utils/types/jwtTypes'
+import type { Request, Response } from "express";
+import * as accountService from "../../../services/admin/accounts/auth.service";
+import * as authService from "../../../services/admin/accounts/auth.service";
+import * as paramsTypes from "../../../utils/types/paramsTypes";
+import type { myJwtPayload } from "../../../utils/types/jwtTypes";
 import type {
-  ResponseDetailSuccess,
-  ResponseFailure,
-} from '../../../utils/types/ResponseTypes'
-import { ResponseListSuccess } from '../../../utils/types/ResponseTypes'
-import ms from 'ms'
+    ResponseDetailSuccess,
+    ResponseFailure,
+} from "../../../utils/types/ResponseTypes";
+import { ResponseListSuccess } from "../../../utils/types/ResponseTypes";
+import ms from "ms";
 export const login = async (req: Request, res: Response) => {
-  try {
-    const { email, password } = req.body
-    const { accessToken, refreshToken, userInfo } = await authService.login(
-      email,
-      password,
-    )
-    
-    res.cookie('accessToken', accessToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'none',
-      maxAge: ms('14 days'),
-    })
+    try {
+        const { email, password } = req.body;
+        const { accessToken, refreshToken, userInfo } = await authService.login(
+            email,
+            password
+        );
 
-    res.cookie('refreshToken', refreshToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'none',
-      maxAge: ms('14 days'),
-    })
-    const response: ResponseDetailSuccess<{
-      // tra ve de FE muon luu vao localStorage thi luu
-      accessToken: string;
-      refreshToken: string;
-      userInfo: myJwtPayload;
-    }> = {
-      code: 200,
-      message: 'Đăng nhập thành công',
-      data: {
-        accessToken,
-        refreshToken,
-        userInfo,
-      },
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: ms("365 days"),
+        });
+
+        res.cookie("refreshToken", refreshToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: ms("365 days"),
+        });
+        const response: ResponseDetailSuccess<{
+            // tra ve de FE muon luu vao localStorage thi luu
+            accessToken: string;
+            refreshToken: string;
+            userInfo: myJwtPayload;
+        }> = {
+            code: 200,
+            message: "Đăng nhập thành công",
+            data: {
+                accessToken,
+                refreshToken,
+                userInfo,
+            },
+        };
+        res.status(200).json(response);
+    } catch (error: any) {
+        const response: ResponseFailure = {
+            code: 400,
+            timestamp: new Date().toISOString(),
+            path: req.path,
+            message: error.message,
+            errors: [],
+        };
+        res.status(400).json(response);
     }
-    res.status(200).json(response)
-  } catch (error: any) {
-    const response: ResponseFailure = {
-      code: 400,
-      timestamp: new Date().toISOString(),
-      path: req.path,
-      message: error.message,
-      errors: [],
-    }
-    res.status(400).json(response)
-  }
-}
+};
 
 export const logout = async (req: Request, res: Response) => {
-  try {
-    // Xoá cookie
-    res.clearCookie('accessToken')
-    res.clearCookie('refreshToken')
-    res.status(200).json({ message: 'Logout API success!' })
-  } catch (error) {
-    res.status(400).json(error)
-  }
-}
+    try {
+        // Xoá cookie
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
+        res.status(200).json({ message: "Logout API success!" });
+    } catch (error) {
+        res.status(400).json(error);
+    }
+};
 
 export const refreshToken = async (req: Request, res: Response) => {
-  try {
-    const refreshTokenFromCookie = req.cookies?.refreshToken
-    const accessToken = await authService.refreshToken(refreshTokenFromCookie)
-    
-    res.cookie('accessToken', accessToken, {
-      httpOnly: true,
-      secure: true,
-      sameSite: 'none',
-      maxAge: ms('14 days'),
-    })
+    try {
+        const refreshTokenFromCookie = req.cookies?.refreshToken;
+        const accessToken = await authService.refreshToken(
+            refreshTokenFromCookie
+        );
 
-    const response: ResponseDetailSuccess<{ accessToken: string; }> = {
-      code: 200,
-      message: 'Refresh Token API success!',
-      data: { accessToken },
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: true,
+            sameSite: "none",
+            maxAge: ms("365 days"),
+        });
+
+        const response: ResponseDetailSuccess<{ accessToken: string }> = {
+            code: 200,
+            message: "Refresh Token API success!",
+            data: { accessToken },
+        };
+        res.status(200).json(response);
+    } catch (error: any) {
+        const response: ResponseFailure = {
+            code: 400,
+            timestamp: new Date().toISOString(),
+            path: req.path,
+            message: error.message || "Refresh Token API Failed",
+            errors: [],
+        };
+        res.status(400).json(response);
     }
-    res.status(200).json(response)
-  } catch (error: any) {
-    const response: ResponseFailure = {
-      code: 400,
-      timestamp: new Date().toISOString(),
-      path: req.path,
-      message: error.message || 'Refresh Token API Failed',
-      errors: [],
-    }
-    res.status(400).json(response)
-  }
-}
+};
